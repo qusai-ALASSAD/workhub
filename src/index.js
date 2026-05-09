5ac8441e0e82e71d65c20fd737b4488a46453c27
qusai als <qosai.web@gmail.com>
2026-05-09 07:05:32 +0200

    Load stable WorkHub shell

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppHotfix.jsx';
+import App from './AppStable.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
